d741a07d64d9692f6f1d3c23acd342c7578b7004
krushnakant <krushnakant@cse.iitb.ac.in>
2020-10-27 22:12:12 +0530

    Internet permission added

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -6,6 +6,7 @@ import 'package:flutter_auth/components/rounded_button.dart';
 import 'package:flutter_auth/components/rounded_input_field.dart';
 import 'package:flutter_auth/components/rounded_password_field.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:http/http.dart' as http;
 
 class Body extends StatelessWidget {
   const Body({
